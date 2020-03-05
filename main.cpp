c8d9fd9a8ecf5c51a1d10db95609c29dc4beead3
PointlessVenture (Aiden Tracy) <aidenrtracy@icloud.com>
2020-03-05 16:18:44 -0500

    Initial Commit and Project Setup

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
+#include <fstream>
+#include <math.h>
+#include <vector>
+#include <list>
+using namespace std;
+
+int main()
+{
+
+	cout << "InitialCode" << endl;
+
+	string noQuit;
+	cin >> noQuit;
+	return 0;
+}
