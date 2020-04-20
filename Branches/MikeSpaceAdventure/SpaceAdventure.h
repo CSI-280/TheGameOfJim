957fe2363b84adfbd56b3a5d3039a11184934bef
Mike <michael.bamford@mymail.champlain.edu>
2020-04-20 12:52:33 -0400

    small file name changes

--- Branches/MikeSpaceAdventure/SpaceAdventure.h
+++ Branches/MikeSpaceAdventure/SpaceAdventure.h
@@ -28,27 +28,39 @@ public:
 	}
 };
 
-void initMikeBranch(unordered_map<string, Room*>* Rooms, unordered_map<string, Link*>* Links, unordered_map<string, Container*>* Containers, Inventory* Inventory)
+class EmptyLinkToContainer : public Link
+{
+public:
+	EmptyLinkToContainer(string cName, Container* cContainer) : Link(cName, cContainer)
+	{
+
+	}
+
+	bool checkConditions()
+	{
+		return true;
+	}
+};
+
+void initSpaceShip(unordered_map<string, Room*>* Rooms, unordered_map<string, Link*>* Links, unordered_map<string, Container*>* Containers, Inventory* Inventory)
 {
 	//Initalize Data
 	Room* spaceShip = new Room("Space Ship");
 	Room* kathor = new Room("Kathor");
 	Room* fentol = new Room("Fentol");
 	Room* tovic = new Room("tovic");
-	
+
 	EmptyLinkToRoom* elToSpaceShip = new EmptyLinkToRoom("Empty Link to Space Ship", spaceShip);
 	EmptyLinkToRoom* elToKathor = new EmptyLinkToRoom("Empty Link to Kathor", kathor);
 	EmptyLinkToRoom* elToFentol = new EmptyLinkToRoom("Empty Link to Fentol", fentol);
 	EmptyLinkToRoom* elToTovic = new EmptyLinkToRoom("Empty Link to Tovic", tovic);
-	
-	
 
 	//Store Data in Maps
 	(*Rooms).insert(pair<string, Room*>((*spaceShip).getName(), spaceShip));
 	(*Rooms).insert(pair<string, Room*>((*kathor).getName(), kathor));
 	(*Rooms).insert(pair<string, Room*>((*fentol).getName(), fentol));
 	(*Rooms).insert(pair<string, Room*>((*tovic).getName(), tovic));
-	
+
 	(*Links).insert(pair<string, Link*>((*elToSpaceShip).getName(), elToSpaceShip));
 	(*Links).insert(pair<string, Link*>((*elToKathor).getName(), elToKathor));
 	(*Links).insert(pair<string, Link*>((*elToFentol).getName(), elToFentol));
@@ -80,21 +92,24 @@ void initMikeBranch(unordered_map<string, Room*>* Rooms, unordered_map<string, L
 	RoomDescription.push_back("It is home to the Kathins, a peaceful race who care more about their crops than outsiders.");
 	(*(*Rooms)["Kathor"]).addRoomDescription(RoomDescription);
 	RoomDescription.clear();
-	RoomDescription.push_back("Stop looking at Kathor");
+	RoomDescription.push_back("Go to Kathor.");
+	RoomDescription.push_back("Stop looking at Kathor.");
 	(*(*Rooms)["Kathor"]).addLinkDescription(RoomDescription);
 	RoomDescription.clear();
 	RoomDescription.push_back("Fentol is a place of a vast jungle consuming the whole planet.");
 	RoomDescription.push_back("It is home to the Fents, a religious race who worship their god, Zutzin.");
 	(*(*Rooms)["Fentol"]).addRoomDescription(RoomDescription);
 	RoomDescription.clear();
-	RoomDescription.push_back("Stop looking at Fentol");
+	RoomDescription.push_back("Go to Fentol.");
+	RoomDescription.push_back("Stop looking at Fentol.");
 	(*(*Rooms)["Fentol"]).addLinkDescription(RoomDescription);
 	RoomDescription.clear();
 	RoomDescription.push_back("Tovic is split in half one side only ocean the other is a enormous desert.");
 	RoomDescription.push_back("It is home to many clans of desert bandits who descended from the Tovir");
 	(*(*Rooms)["Tovic"]).addRoomDescription(RoomDescription);
 	RoomDescription.clear();
-	RoomDescription.push_back("Stop looking at Tovic");
+	RoomDescription.push_back("Go to Tovic.");
+	RoomDescription.push_back("Stop looking at Tovic.");
 	(*(*Rooms)["Tovic"]).addLinkDescription(RoomDescription);
-
+}
 
